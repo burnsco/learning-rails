7d3d93a09eab19284685e5ce5606aa7949d51877
Corey Burns <coreyburns@outlook.com>
2024-09-09 10:52:49 -0400

    added a render html def on app controller then added to routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  def posts
+    render html: "Posts Page"
+  end
+
+
   allow_browser versions: :modern
 end
